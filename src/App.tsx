1a5ed056b13bf90ab17f820968b9d06ea244860d
Colby Rehn <rehn.colby@gmail.com>
2023-05-28 16:24:38 +1000

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  );
+}
+
+export default App;
